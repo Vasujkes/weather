8c7a8898fe0d0c5ecf699e82eb12ebe2bfe31c35
Vasujke <dev.vasujke@gmail.com>
2020-08-08 22:42:39 +0300

    Updates

---
+++ precache-manifest.07a8ea940b46bd3ae0b5360e99790906.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d462f4f14f9fa55524a0c4019c0b34b",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "3dbece082e1f31d42c93",
+    "url": "/weather/static/css/main.d7c2b87d.chunk.css"
+  },
+  {
+    "revision": "0dbb2b44e395af8e229f",
+    "url": "/weather/static/js/2.87103b21.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather/static/js/2.87103b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dbece082e1f31d42c93",
+    "url": "/weather/static/js/main.4a5b9123.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "3e687346552a79cd825f343fe1193a84",
+    "url": "/weather/static/media/cloudy.3e687346.svg"
+  },
+  {
+    "revision": "9758264fe77c807f1bff233e4481bf9c",
+    "url": "/weather/static/media/drizzle.9758264f.svg"
+  },
+  {
+    "revision": "92a8f1aba1f45fca376642f5880864d8",
+    "url": "/weather/static/media/showers.92a8f1ab.svg"
+  },
+  {
+    "revision": "910a52275bb49afa4b59639ab067029b",
+    "url": "/weather/static/media/snow.910a5227.svg"
+  },
+  {
+    "revision": "99453e02c7132af0f385b03fec8c05c0",
+    "url": "/weather/static/media/sunny.99453e02.svg"
+  },
+  {
+    "revision": "676a6c2afbea9b56411f4a4bd27075b3",
+    "url": "/weather/static/media/thunderstroms.676a6c2a.svg"
+  },
+  {
+    "revision": "955b65ff3718c0e146a53cb35efce2c8",
+    "url": "/weather/static/media/tornado.955b65ff.svg"
+  }
+]);
